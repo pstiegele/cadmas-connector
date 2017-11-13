cce2789a608a562053feeb66a22a0a1cc3e8dbdb
pstiegele <paul@stiegele.name>
2017-11-13 18:58:37 +0100

    project created

---
+++ src/controller/Main.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//connect to server
+		SocketConnection socketConnection = new SocketConnection();
+		//connect to autopilot
+
+	}
+
+}
