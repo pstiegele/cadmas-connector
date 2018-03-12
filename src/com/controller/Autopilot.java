9b0cf5444e6c648852df963c09fbad1d339a584f
pstiegele <paul@stiegele.name>
2018-03-12 18:37:48 +0100

    syso for everyone!

--- src/com/controller/Autopilot.java
+++ src/com/controller/Autopilot.java
@@ -15,10 +15,16 @@ public class Autopilot {
 	public boolean connect(SocketConnection socketConnection) {
 		this.socketConnection=socketConnection;
 		//initialize
+		System.out.println("start init");
 		SerialPort port = init();
+		System.out.println("init finished");
 		while(true) {
+			System.out.println("start getPacket");
 			MAVLinkPacket mavpacket = getPacket(port);
+			System.out.println("finished getPacket");
+			System.out.println("start handlePacket");
 			handlePacket(mavpacket);
+			System.out.println("finished handlePacket");
 		}
 		
 		
@@ -28,10 +34,11 @@ public class Autopilot {
 	
 	
 	private void handlePacket(MAVLinkPacket mavpacket) {
+		System.out.println("mavpacket id: "+mavpacket.msgid);
 		switch (mavpacket.msgid) {
 		case msg_heartbeat.MAVLINK_MSG_ID_HEARTBEAT:
 			Heartbeat hb = new Heartbeat(mavpacket);
-			//socketConnection.send(hb);
+			socketConnection.send(hb);
 			break;
 		case msg_attitude.MAVLINK_MSG_ID_ATTITUDE:
 			msg_attitude att = new msg_attitude(mavpacket);
@@ -58,20 +65,25 @@ public class Autopilot {
 		Parser parser = new Parser();
 		try {
 			while (true) {
+				System.out.println("wait for bytes");
 				while (port.bytesAvailable() == 0)
 					Thread.sleep(20);
 				int bytesToRead = port.bytesAvailable();
+				System.out.println("there are "+bytesToRead+" bytes to read");
 				byte[] readBuffer = new byte[bytesToRead];
+				System.out.println("read: "+readBuffer.toString());
 				port.readBytes(readBuffer, bytesToRead);
 				int readarr[] = new int[bytesToRead];
 				for (int i = 0; i < readBuffer.length; i++) {
 					readarr[i] = unsignedToBytes(readBuffer[i]);
 				}
-
+				System.out.println("unsignedToBytes: "+readarr.toString());
 				MAVLinkPacket mavpacket = null;
 				for (int i = 0; i < readarr.length; i++) {
+					System.out.println("parse char: "+readarr[i]);
 					mavpacket = parser.mavlink_parse_char(readarr[i]);
 					if (mavpacket != null) {
+						System.out.println("mavpacket found!!!");
 						return mavpacket;
 						
 					}
@@ -98,6 +110,7 @@ public class Autopilot {
 		}
 		SerialPort port = ports[0];
 		port.openPort();
+		System.out.println("port is open: "+port.getBaudRate()+"baud. "+port.getDescriptivePortName());
 		return port;
 	}
 	
