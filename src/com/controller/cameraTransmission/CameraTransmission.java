32f65c8e2180373dab1f4d39639a58fc095ee76a
pstiegele <paul@stiegele.name>
2018-07-25 17:20:57 +0200

    next cam param try

--- src/com/controller/cameraTransmission/CameraTransmission.java
+++ src/com/controller/cameraTransmission/CameraTransmission.java
@@ -36,7 +36,7 @@ public class CameraTransmission extends Thread {
 		RPiCamera cam = null;
 		try {
 			cam = new RPiCamera();
-			cam.setHeight(200).setWidth(300).setQuality(90).setExposure(Exposure.SPORTS);
+			cam.setHeight(200).setWidth(300).setQuality(90).setExposure(Exposure.SPORTS).setTimeout(100);
 		} catch (FailedToRunRaspistillException e) {
 			System.err.println("No Raspberry Pi camera found.");
 		}
